bfd903cfd16ea7bbdeb6ddbff945295006542179
Ted Kim <tkk21@case.edu>
2014-11-03 04:08:36 -0500

    made a method regarding illegal string input and how to end when the input isn't a correct chemistry syntax

--- src/chemistry/ExceptionUtils.java
+++ src/chemistry/ExceptionUtils.java
@@ -19,4 +19,27 @@ public class ExceptionUtils {
 			}
 		}
 	}
+	
+	public static void checkIllegalString (String s){
+		try{
+			ExceptionUtils.checkNulls(s);//throw NPE if input is null
+		}
+		catch(NullPointerException e){
+			failChemistry();
+		}
+		if (s.length()==0){
+			failChemistry();
+		}
+	}
+	
+	/**
+	 * a possible room for hacking
+	 * is this a safe thing to do?
+	 * 
+	 * can someone prematurely exit the program using this?  
+	 */
+	private static void failChemistry (){
+		System.out.println("F");
+		System.exit(0);//what status to exit with?
+	}
 }
