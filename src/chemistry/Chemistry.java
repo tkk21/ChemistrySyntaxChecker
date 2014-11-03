3536819b2dd25ededd20181933cca0275394784a
Ted Kim <tkk21@case.edu>
2014-11-03 03:38:27 -0500

    initial commit added notes to chemistry

---
+++ src/chemistry/Chemistry.java
@@ -0,0 +1,42 @@
+package chemistry;
+
+/**
+ * hw10
+ * 
+ * @author ted
+ *
+ */
+public class Chemistry {
+
+	//defensive programming so be really careful
+	
+	//not required to test
+	
+	//watch out for exceptions being thrown in the constructor
+	
+	/**
+	 * Valid element names have
+	 * zero to two more lower case letters
+	 * 
+	 * so Three characters in total allowed
+	 * Hello <- not valid
+	 * Jar <- valid
+	 */
+	
+	/**
+	 * 
+	 * @param args
+	 */
+
+	public static void main(String [] args){
+		//sysout "T" if syntax is correct
+		//sysout "F" is syntax is incorrect
+		
+		//care about defensive programming when writing this
+		/**
+		 * make sure main is the only entry point to chemistry
+		 */
+		//document error handling architecture
+	}
+
+}
