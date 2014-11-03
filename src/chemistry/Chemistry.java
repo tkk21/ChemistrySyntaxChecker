adc359f1c72a5cf82ff9f558d9cf53dd40c81d2f
Ted Kim <tkk21@case.edu>
2014-11-03 03:43:54 -0500

    added isElement method

--- src/chemistry/Chemistry.java
+++ src/chemistry/Chemistry.java
@@ -14,6 +14,8 @@ public class Chemistry {
 	
 	//watch out for exceptions being thrown in the constructor
 	
+
+	
 	/**
 	 * Valid element names have
 	 * zero to two more lower case letters
@@ -21,8 +23,23 @@ public class Chemistry {
 	 * so Three characters in total allowed
 	 * Hello <- not valid
 	 * Jar <- valid
+	 * @param s
+	 * @return
 	 */
-	
+	public static boolean isElement(String s){
+		if (s.length()>3){
+			return false;
+		}
+		if (!Character.isUpperCase(s.charAt(0))){
+			return false;
+		}
+		for (int i = 1; i<s.length(); i++){
+			if (Character.isUpperCase(s.charAt(i))){
+				return false;
+			}
+		}
+		return true;
+	}
 	/**
 	 * 
 	 * @param args
