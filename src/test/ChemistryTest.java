d97ced2ee354775ee3cf11ad684ae1e5191c8a21
Ted Kim <tkk21@case.edu>
2014-11-03 03:52:19 -0500

    wrote some test cases comment

--- src/test/ChemistryTest.java
+++ src/test/ChemistryTest.java
@@ -5,10 +5,23 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class ChemistryTest {
-
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
-
+	
+	/**
+	 * cases
+	 * 1. single element
+	 * H
+	 * 1a. single element with length 2
+	 * He
+	 * 1b. single element with length 3
+	 * Heh
+	 * 2. single element with coefficient
+	 * H3
+	 * 3. single element within parenthesis
+	 * (H)
+	 * 4. single element within parenthesis with coefficient
+	 * (H)2
+	 * 5. single element with coefficient within parenthesis with coefficient
+	 * (H2)4 
+	 */
+	
 }
