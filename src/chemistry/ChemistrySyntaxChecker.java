99c5e961ab77459e38afeab38071ec60b9d8d6d3
Ted Kim <tkk21@case.edu>
2014-11-06 18:41:51 -0500

    commit before switching over to enum for booleans

--- src/chemistry/ChemistrySyntaxChecker.java
+++ src/chemistry/ChemistrySyntaxChecker.java
@@ -3,24 +3,43 @@ package chemistry;
 
 public class ChemistrySyntaxChecker {
 
-	private boolean hasParenthesis;
-	private int parenthesisCount;
-	private boolean justClosedParenthesis;
+	public enum ChemistryCharacterStatus{
+		firstCharacter, openParenthesis, closedParenthesis, 
+		upperLetter, lowerLetter, number;
+	}
 	
-	private boolean hasLetter;
+	private boolean firstCharacter;
+	
+	private int parenthesisCount;
+	
+	private boolean afterOpenParenthesis; 
+	private boolean afterClosedParenthesis;
+	
+	private boolean afterLetter;
+	private boolean afterStartElement;
+	
+	private ChemistryCharacterStatus beforeCharacter;
 	private int letterCount;
-	private boolean hasNumber;
+	
+	private boolean afterNumber;
+	
 	private static final String NOT_CHEMICALLY_VALID = "[^a-zA-Z0-9(-)]";
 	
+
 	public ChemistrySyntaxChecker () {
-		hasParenthesis = false;
-		hasLetter = false;
-		hasNumber = false;
+		
+		beforeCharacter = ChemistryCharacterStatus.firstCharacter;
+		
+		firstCharacter = true;
 		
 		parenthesisCount = 0;
 		letterCount = 0;
 		
-		justClosedParenthesis = false;
+		afterClosedParenthesis = false;
+		afterLetter = false;
+		afterStartElement = false;
+		
+		afterNumber = false;
 	}
 	
 	public void checkSyntax(String s) {
@@ -31,8 +50,53 @@ public class ChemistrySyntaxChecker {
 			checkChar(s.charAt(i));
 		}
 	}
+	private void processChemistrySyntax(){
+		switch(beforeCharacter){
+		case firstCharacter:
+			
+		}
+	}
+	private void legalChars(){
+		if (firstCharacter){
+			firstCharacter = false;
+			//anything allowed
+		}
+		if (afterStartElement){
+			//capital letter CH
+			//lower letter Co
+			//number H2
+			//parenthesis H(O)
+		}
+		if (afterLetter){
+			//capital letter HeO
+			//another letter Hea
+			//number He2
+			//parenthesis He(O)
+		}
+		if (afterNumber){
+			//capital letter H2O
+			//number H23
+			//parenthesis H2(O)
+		}
+		if (afterOpenParenthesis){
+			//capital letter (H)
+			//number (3Co)
+			//parenthesis ((He))
+		}
+		if (afterClosedParenthesis){
+			//capital letter (He)O
+			//number (He)3
+			//parenthesis (He)(Ho)
+		}
+	}
 	
+	private void startElement(){
+		
+	}
 	private void checkChar (char c){
+		if (afterStartElement){
+			
+		}
 		switch(c){
 		//parenthesis case
 		case '(':
@@ -48,7 +112,7 @@ public class ChemistrySyntaxChecker {
 			}
 			else{
 				parenthesisCount--;
-				justClosedParenthesis = true;
+				afterClosedParenthesis = true;
 			}
 			break;
 			
@@ -60,8 +124,8 @@ public class ChemistrySyntaxChecker {
 	
 	private void checkCompoundValidity(char c){
 		
-		
 	}
+	
 	private String sanitize(String s) {
 		String sanitized = s;
 		sanitized = sanitized.replaceAll(NOT_CHEMICALLY_VALID, "");
