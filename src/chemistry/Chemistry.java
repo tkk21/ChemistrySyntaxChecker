1582a7d00ac41ffefefcf52d7b13ee4aa04398da
Ted Kim <tkk21@case.edu>
2014-11-07 02:06:46 -0500

    got rid of obsolete methods

--- src/chemistry/Chemistry.java
+++ src/chemistry/Chemistry.java
@@ -14,41 +14,6 @@ public class Chemistry {
 
 	//watch out for exceptions being thrown in the constructor
 
-	public static void parseChemistry(String s){
-		ExceptionUtils.checkIllegalString(s);
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i<s.length(); i++){
-			
-			sb.append(s.charAt(i));
-			
-		}
-	}
-	/**
-	 * finds whether an element name is valid or not
-	 * Valid element names have
-	 * zero to two more lower case letters
-	 * 
-	 * so Three characters in total allowed
-	 * Hello <- not valid
-	 * Jar <- valid
-	 * @param s
-	 * @return
-	 */
-	public static boolean isElement(String s){
-		if (s.length()>3){
-			return false;
-		}
-		if (!Character.isUpperCase(s.charAt(0))){
-			return false;
-		}
-		for (int i = 1; i<s.length(); i++){
-			if (Character.isUpperCase(s.charAt(i))){
-				return false;
-			}
-		}
-		return true;
-	}
-
 	/**
 	 * 
 	 * @param args
