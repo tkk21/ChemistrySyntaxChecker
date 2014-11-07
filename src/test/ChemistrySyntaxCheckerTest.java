eade531ff7885a403d2ea42c061e34524760a1cd
Ted Kim <tkk21@case.edu>
2014-11-07 04:02:11 -0500

    moved the comment notes from chemistry test to chemistry syntax checker test

--- src/test/ChemistrySyntaxCheckerTest.java
+++ src/test/ChemistrySyntaxCheckerTest.java
@@ -19,8 +19,27 @@ public class ChemistrySyntaxCheckerTest {
 	@Before
 	public void init(){
 		c = new ChemicalSyntaxChecker();
-		
 	}
+	
+	/**
+	 * cases
+	 * 1. single element
+	 * H
+	 * 1a. single element with length 2
+	 * He
+	 * 1b. single element with length 3
+	 * Heh
+	 * 2. single element with coefficient
+	 * H3
+	 * 3. single element within parenthesis
+	 * (H)
+	 * 4. single element within parenthesis with coefficient
+	 * (H)2
+	 * 5. single element with coefficient within parenthesis with coefficient
+	 * (H2)4 
+	 */
+	
+	
 	@Test
 	public void testParenthesis() throws IllegalElementException, IllegalParenthesisException {
 		c.checkSyntax("(H)");
